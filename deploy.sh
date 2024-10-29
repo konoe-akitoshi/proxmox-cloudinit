d42b75ae2b85096bbebf8b22718bce46626859fe
csenet <admin@k1h.dev>
2024-10-29 13:24:31 +0900

    update script

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ VLAN_TAG=$6
 # Check arguments and show usage
 if [ $# -lt 5 ]; then
   echo "Invalid number of arguments"
-  echo "Usage: ./deploy.sh <VM_ID> <VM_NAME> <GITHUB_ACCOUNT> <PASSWORD> <NETWORK> (<VLAN_TAG>)"
-  echo "Example: ./deploy.sh 9000 test csenet password123 ip=192.168.200.10/24,gw=192.168.200.1 200"
+  echo "Usage: ./deploy.sh <TEMPLATE_VM_ID> <VM_ID> <VM_NAME> <GITHUB_ACCOUNT> <PASSWORD> <NETWORK> (<VLAN_TAG>)"
+  echo "Example: ./deploy.sh 9000 100 test csenet password123 ip=192.168.200.10/24,gw=192.168.200.1 200"
   exit 1
 fi
 
