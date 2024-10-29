f50c1473cbbc5cd860a3f972399beacad6a42376
csenet <admin@k1h.dev>
2024-10-29 13:59:04 +0900

    update cpu setting

--- setup.sh
+++ setup.sh
@@ -17,13 +17,19 @@ MEMORY_SIZE=$3 # Memory Size
 apt-get install cloud-init
 # download the latest Ubuntu Cloud Image
 wget https://cloud-images.ubuntu.com/${UBUNTU_CODE_NAME}/current/${UBUNTU_CODE_NAME}-server-cloudimg-amd64.img
-exit 0
 # create a new VM with VirtIO SCSI controller
-qm create ${VM_ID} --memory ${MEMORY_SIZE} --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci
+qm create ${VM_ID} \ 
+--memory ${MEMORY_SIZE} \
+--net0 virtio,bridge=vmbr0 \
+--scsihw virtio-scsi-pci
+--cores 2 \
+--sockets 1
 # import the downloaded disk to the local-lvm storage, attaching it as a SCSI drive
 qm set ${VM_ID} --scsi0 local-lvm:0,import-from=/root/${UBUNTU_CODE_NAME}-server-cloudimg-amd64.img
 # Add Cluod init CD-ROM
 qm set ${VM_ID} --ide2 local-lvm:cloudinit
+# Change disk size (2GB -> 20GB)
+qm resize ${VM_ID} scsi0 +18G
 # Set boot order
 qm set ${VM_ID} --boot order=scsi0
 # add serial
