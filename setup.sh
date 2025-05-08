30baca82a97dfc50c5002f5c31fb6e96d125be8f
Akitoshi Saeki <51017077+konoe-akitoshi@users.noreply.github.com>
2025-05-08 18:51:21 +0900

    fix: LVMストレージに対応するようにディスクフォーマットを修正 - qcow2フォーマット指定を削除し、デフォルトのrawフォーマットを使用 - EFIディスクの設定も同様に修正

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ virt-customize -a /root/${UBUNTU_CODE_NAME}-server-cloudimg-amd64.img --install 
 # create a new VM with VirtIO SCSI controller
 qm create ${VM_ID} --memory ${MEMORY_SIZE} --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci --cores 2 --sockets 1 --name ubuntu-${UBUNTU_CODE_NAME}-template --machine q35
 # import the downloaded disk to the DISK_POOL storage, attaching it as a SCSI drive
-qm set ${VM_ID} --scsi0 ${DISK_POOL}:0,discard=on,format=qcow2,import-from=/root/${UBUNTU_CODE_NAME}-server-cloudimg-amd64.img
+qm set ${VM_ID} --scsi0 ${DISK_POOL}:0,discard=on,import-from=/root/${UBUNTU_CODE_NAME}-server-cloudimg-amd64.img
 # Add Cloud init CD-ROM
 qm set ${VM_ID} --ide2 ${DISK_POOL}:cloudinit
 # Set disk size to 20G
@@ -49,7 +49,7 @@ qm set ${VM_ID} --boot order=scsi0
 # add serial
 qm set ${VM_ID} --serial0 socket --vga serial0
 # Set EFI disk
-qm set ${VM_ID} --efidisk0 ${DISK_POOL}:1,format=qcow2,efitype=4m,pre-enrolled-keys=1
+qm set ${VM_ID} --efidisk0 ${DISK_POOL}:1,efitype=4m,pre-enrolled-keys=1
 # convert to template
 qm template ${VM_ID}
 
